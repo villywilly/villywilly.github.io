aa0bb98ec137957a47c65d93fe23491769b1da60
villywilly <willblackwood017@gmail.com>
2025-12-07 10:27:45 -0800

    Buttons and games also fixed js
    Added buttons so its easier to read, fixed the fake order form.
    And working on a game thing

--- orderForm.js
+++ orderForm.js
@@ -1,13 +1,24 @@
 
 
-
-// this doesnt actually do anything with entered information
-// its supposed to set 2 headers in an html file to the values users input
-// but right now it does nothing. other than prompt you
-console.log('Remember this whole thing is a joke, but dont put any personal information just in case!');
-console.log('Hey, stop snooping around in my website!!');
-let age = window.prompt('Enter your age. You must be over 18 to order Magic Dirt');
-let address = window.prompt('Enter your address');
-document.getElementById('age').textContent = age;
+// welcome to special area of macig dir website!!
+console.log('Remember this whole thing is a joke, but dont put any personal information just in case!'); // idk the user cant read this unless the user
+console.log('Hey, stop snooping around in my website!!'); // opens inspect element
+let age = window.prompt('Enter your age. You must be over 18 to order Magic Dirt'); // asks for ur age
+let address = window.prompt('Enter your address'); // asks for ur address
+document.getElementById('age').textContent = age; // sets a text element to ur age, other one makes it address
 document.getElementById('address').textContent = address;
-console.log(age,address,'entered');
+console.log(age,address,'entered'); // logs it in console because testing
+let numAge = Number(age);
+if (isNaN(numAge)) { // check if you entered a proper number
+    console.log('Warning! Invalid number error has occured!');
+    window.alert('Warning! Invalid number error has occured! Check console for more details');
+    console.log(`The number the user entered <${age}>, returns as <${numAge}, which is invalid!`);
+}
+else if(numAge < 18) { // check if your old enough
+    console.log(`Warning! Use too young! Entered age <${numAge} returns below 18, so is not allowed to purchase magic dirt!`);
+    window.alert('Sorry, you must be over 18 to purchase this product.');
+}
+else { // say purchase in progress
+    console.log('Success! purchase is in progress.');
+    window.alert('Success! Order in progress');
+}
