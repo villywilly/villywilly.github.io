406e0e43ae53c0883d10854fc973f3570aa193ca
villywilly <willblackwood017@gmail.com>
2026-01-29 17:40:00 -0800

    Update passCheck.js

--- secret area/passCheck.js
+++ secret area/passCheck.js
@@ -3,10 +3,11 @@ function check() {
     let password = document.getElementById('pass').value;
     if (password===passCheck) {
         window.alert('Congrats, you have been granted access to the file.')
+        window.alert('JK, access denied bc nobody can see this anymore bc idk just yea')
         console.log('Password confirmed, redirecting user...')
-        window.location.href = 'https://drive.google.com/drive/folders/1wvhH_-PRjJHU7AP9Za_4s04f1SMR7Lrd?usp=sharing';
     }
     else {
         window.alert('Password incorrect, try again! Password hint: Only a true hacker would know.');
     }
+
 }
